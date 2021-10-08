345f28f1ea3761939e0875db67cc7dab4aaac009
xubury <xubury97@gmail.com>
2021-10-08 15:50:09 +0800

    * fix removeSystem bug

--- Engine/src/Core/Layer.cpp
+++ Engine/src/Core/Layer.cpp
@@ -5,9 +5,9 @@
 namespace sd {
 
 void Layer::removeSystem(const Ref<System> &system) {
-    system->onDestroy();
     auto iter = std::find(m_systems.begin(), m_systems.end(), system);
     if (iter != m_systems.end()) {
+        system->onDestroy();
         m_systems.erase(iter);
     } else {
         SD_CORE_WARN("System does not exist!");
