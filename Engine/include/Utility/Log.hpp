19cf74320e9437399c1100b9d976a0d9f110038a
xubury <xubury97@gmail.com>
2021-11-24 12:33:00 +0800

    * glm log

--- Engine/include/Utility/Log.hpp
+++ Engine/include/Utility/Log.hpp
@@ -3,6 +3,10 @@
 
 #include "Utility/Export.hpp"
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
+#include <glm/glm.hpp>
+
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
 
@@ -10,11 +14,11 @@ namespace SD {
 
 class SD_API Log {
    public:
-    static void Init(const std::string &logFileName);
+    static void Init(const std::string& logFileName);
 
-    static std::shared_ptr<spdlog::logger> &GetCoreLogger();
+    static std::shared_ptr<spdlog::logger>& GetCoreLogger();
 
-    static std::shared_ptr<spdlog::logger> &GetClientLogger();
+    static std::shared_ptr<spdlog::logger>& GetClientLogger();
 
    private:
     static std::shared_ptr<spdlog::logger> s_coreLogger;
@@ -22,6 +26,46 @@ class SD_API Log {
 };
 
 }  // namespace SD
+
+template <glm::length_t L, typename T, glm::qualifier Q>
+struct fmt::formatter<glm::vec<L, T, Q>> {
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        return ctx.end();
+    }
+
+    template <typename FormatContext>
+    auto format(const glm::vec<L, T, Q>& input, FormatContext& ctx)
+        -> decltype(ctx.out()) {
+        return format_to(ctx.out(), glm::to_string(input));
+    }
+};
+
+template <typename T, glm::qualifier Q>
+struct fmt::formatter<glm::qua<T, Q>> {
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        return ctx.end();
+    }
+
+    template <typename FormatContext>
+    auto format(const glm::qua<T, Q>& input, FormatContext& ctx)
+        -> decltype(ctx.out()) {
+        return format_to(ctx.out(), "quat({}, {}, {}, {})", input.w, input.x,
+                         input.y, input.z);
+    }
+};
+
+template <glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+struct fmt::formatter<glm::mat<C, R, T, Q>> {
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        return ctx.end();
+    }
+
+    template <typename FormatContext>
+    auto format(const glm::mat<C, R, T, Q>& input, FormatContext& ctx)
+        -> decltype(ctx.out()) {
+        return format_to(ctx.out(), glm::to_string(input));
+    }
+};
 
 #define SD_CORE_TRACE(...) ::SD::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define SD_CORE_INFO(...) ::SD::Log::GetCoreLogger()->info(__VA_ARGS__)
