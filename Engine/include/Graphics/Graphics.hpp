09fc4f6745a1fcdc1d43db8f9a85fe98829610fa
xubury <xubury97@gmail.com>
2021-11-26 20:02:11 +0800

    - name format

--- Engine/include/Graphics/Graphics.hpp
+++ Engine/include/Graphics/Graphics.hpp
@@ -98,13 +98,13 @@ struct SD_API RGBA {
    public:
     RGBA(float r, float g, float b, float a) : m_color{r, g, b, a} {}
 
-    float red() { return m_color[0]; }
-    float green() { return m_color[1]; }
-    float blue() { return m_color[2]; }
-    float alpha() { return m_color[3]; }
+    float Red() { return m_color[0]; }
+    float Green() { return m_color[1]; }
+    float Blue() { return m_color[2]; }
+    float Alpha() { return m_color[3]; }
 
-    float *data() { return m_color.data(); }
-    const float *data() const { return m_color.data(); }
+    float *Data() { return m_color.data(); }
+    const float *Data() const { return m_color.data(); }
 
    private:
     std::array<float, 4> m_color;
