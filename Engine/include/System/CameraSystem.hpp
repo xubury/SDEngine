fd3dcc9fad4e7ae21d3a203e46acdf0782095f88
xubury <xubury97@gmail.com>
2021-12-06 10:48:36 +0800

    * fix compliation

--- Engine/include/System/CameraSystem.hpp
+++ Engine/include/System/CameraSystem.hpp
@@ -1,11 +1,12 @@
 #ifndef SD_CAMERA_SYSTEM_HPP
 #define SD_CAMERA_SYSTEM_HPP
 
+#include "System/Export.hpp"
 #include "Core/System.hpp"
 
 namespace SD {
 
-class CameraSystem : public System {
+class SD_SYSTEM_API CameraSystem : public System {
    public:
     CameraSystem();
 
