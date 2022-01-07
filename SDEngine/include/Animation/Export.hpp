0e8b0f163d2432fbae31b8e0d2061ab973a7785d
xubury <xubury97@gmail.com>
2022-01-07 12:09:47 +0800

    * add animation module

---
+++ SDEngine/include/Animation/Export.hpp
@@ -0,0 +1,16 @@
+#ifndef SD_ANIMATION_EXPORT_HPP
+#define SD_ANIMATION_EXPORT_HPP
+
+#include "Utility/Config.hpp"
+
+#if defined(SD_ANIMATION_EXPORT)
+#define SD_ANIMATION_API SD_API_EXPORT
+#define ENTT_API_EXPORT
+#elif defined(SD_ANIMATION_IMPORT)
+#define SD_ANIMATION_API SD_API_IMPORT
+#define ENTT_API_EXPORT
+#else
+#define SD_ANIMATION_API
+#endif
+
+#endif // !SD_ANIMATION_EXPORT_HPP
