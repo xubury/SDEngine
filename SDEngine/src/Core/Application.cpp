f1c0e53ad4e0e73a346589502899433c30ccb47b
xubury <xubury97@gmail.com>
2022-03-23 18:05:28 +0800

    * update imgui

--- SDEngine/src/Core/Application.cpp
+++ SDEngine/src/Core/Application.cpp
@@ -23,19 +23,15 @@ std::filesystem::path Application::GetAppDirectory()
 {
     static std::filesystem::path path;
     if (path.empty()) {
-#if defined(SD_PLATFORM_LINUX)
-        char buffer[260];
-        size_t size = readlink("/proc/self/exe", buffer, sizeof(buffer));
-        if (size > 0) {
-            path.assign(std::begin(buffer), std::begin(buffer) + size);
-        }
-#elif defined(SD_PLATFORM_WINDOWS)
         char buffer[255];
+#if defined(SD_PLATFORM_LINUX)
+        size_t size = readlink("/proc/self/exe", buffer, sizeof(buffer));
+#elif defined(SD_PLATFORM_WINDOWS)
         size_t size = GetModuleFileNameA(NULL, buffer, sizeof(path));
+#endif
         if (size > 0) {
             path.assign(std::begin(buffer), std::begin(buffer) + size);
         }
-#endif
     }
     return path.parent_path();
 }
